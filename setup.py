95eb8476ca48b54102b69ad18123875503e3495e
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-09-27 15:06:57 +0300

    Updated Dictionary repository translation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-Dictionary',
+    version='1.0.1',
+    packages=['Language', 'resources', 'Dictionary', 'Dictionary.Trie', 'Syllibification'],
+    url='https://github.com/olcaytaner/Dictionary-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Simple Dictionary Processing'
+)
