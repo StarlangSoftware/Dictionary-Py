8cb08a840146272571332aadb40a71c6bc0d05f9
olcaytaner <olcaytaner@isikun.edu.tr>
2019-10-10 16:26:05 +0300

    Added misspelled word file and its processing.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-Dictionary',
-    version='1.0.1',
+    version='1.0.2',
     packages=['Language', 'resources', 'Dictionary', 'Dictionary.Trie', 'Syllibification'],
     url='https://github.com/olcaytaner/Dictionary-Py',
     license='',
