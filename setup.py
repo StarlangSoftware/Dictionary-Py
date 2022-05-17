bc50ec63dedb7c2a4ab942e498b6ccedc425dee0
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 22:50:05 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-Dictionary',
-    version='1.0.26',
+    version='1.0.27',
     packages=['Language', 'Dictionary', 'Dictionary.data', 'Dictionary.Trie', 'Syllibification'],
     package_data={'Dictionary.data': ['*.txt']},
     url='https://github.com/StarlangSoftware/Dictionary-Py',
